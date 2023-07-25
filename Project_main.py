f22c55dbc06145c3cda8544179e797acb030928f
bzman101 <40146242+bzman101@users.noreply.github.com>
2023-07-25 15:46:08 +0300

    Change file name and add first function

--- Project_main.py
+++ Project_main.py
@@ -1,2 +1,16 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sys
+
+##Functions
+
+def df_cleanup(df):
+    """
+    This Function receives a df of a freq file returns the same df with 3 columns extra:
+    MOI - presenting the MOI of the experiment
+    Passage - indicating the passage of each mutation
+    Done_By - indicating the scientist responsible for the experiment
+    """
+    return df_arranged
