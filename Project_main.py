ea7d6376f12ad4c8718aaf12fee6c638b94c28ac
bzman101 <40146242+bzman101@users.noreply.github.com>
2023-07-25 15:48:04 +0300

    Change file name and add first function

--- Project_main.py
+++ Project_main.py
@@ -8,9 +8,12 @@ import sys
 
 def df_cleanup(df):
     """
+    :param:df
     This Function receives a df of a freq file returns the same df with 3 columns extra:
     MOI - presenting the MOI of the experiment
     Passage - indicating the passage of each mutation
     Done_By - indicating the scientist responsible for the experiment
+    :return: df_arranged
     """
+
     return df_arranged
