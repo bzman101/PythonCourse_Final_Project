9c7244d23a66a7c36b9b61d1ecb24e8e71034896
bzman101 <40146242+bzman101@users.noreply.github.com>
2023-07-31 12:53:54 +0300

    added get_mut_list and mut_cutoffs

--- Project_main.py
+++ Project_main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import sys
 import re
+from MS2_COLORS import COLORS
 
 ## Functions
 
@@ -28,31 +29,38 @@ def df_cleanup(df,done_by):
     return df_arranged
 def get_mut_list(merged_df):
     """
-    This Function receives a merged df of freq files returns a new df containing the following:
-    Position - the position of the nucleotide in query
-    Ref_nuc - The nucleotide present in the reference sequence
-    Mut_nuc - The nucleotide present in the mutative sequence
+    This Function receives a merged df of freq files adds a Mutation field:
     Mutation - the mutation written in a formatted way :(N_ref+position+N_mut)
-    Coverage - How many reads where mapped to that mutation
-    Avg_Score, Frequency, MOI, Passage, Done_By
-    The new df will be cleaned up from all Null
-    return: df_arranged
+    In addition, the function will lose mutation from known problematic regions or primer region.
+    return: mut_df
     """
-
-    #return mut_df
+    # Add Mutation column
+    mut_df = merged_df.copy()
+    mut_df['Full Mutation'] = mut_df['ref_base'] + mut_df['ref_pos'].astype(str) + mut_df['read_base']
+    # Lose problematic lines (Primers and Region of
+    primers = list(range(1, 20)) + list(range(1291, 1304)) + list(range(1179, 1200)) + list(range(2270, 2288)) + \
+              list(range(2167, 2188)) + list(range(3548, 3570))
+    problematic = [17, 18, 19, 20, 21, 22, 23, 183, 188, 189, 190, 196, 274, 317, 364, 452, 762, 2719, 3117, 3133, 3139,
+                   3143, 3146, 3150, 3401, 3539, 3542]
+    remove_list = primers + problematic
+    mut_df = mut_df[~joined_freq['ref_pos'].isin(remove_list)]
+    return mut_df
 
 def mut_cutoffs(mut_df,min_coverage,min_freq):
     """
     This Function receives a mutants df, a minimum coverage value and a minimum frequency value.
     It returns the same df where the mutants that does not meet the criteria are dropped.
     """
+    filtered_mut_df = mut_df.copy()
 
     #return filtered_mut_df
 
 ## Constants and Parameters
-#maria_path = "/Users/adibnzv/Desktop/DESKTOP/SCHOOL/PhD/Year 1/Semester 2/Python for Biologists/Final Project/PythonCourse_Final_Project/DATA/Maria/Maria1.csv"
+maria_path = "/Users/adibnzv/Desktop/DESKTOP/SCHOOL/PhD/Year 1/Semester 2/Python for Biologists/Final Project/PythonCourse_Final_Project/DATA/Maria/Maria1.csv"
 carmel_path = "/Users/adibnzv/Desktop/DESKTOP/SCHOOL/PhD/Year 1/Semester 2/Python for Biologists/Final Project/PythonCourse_Final_Project/DATA/Carmel/Carmel1.csv"
 shir_path = "/Users/adibnzv/Desktop/DESKTOP/SCHOOL/PhD/Year 1/Semester 2/Python for Biologists/Final Project/PythonCourse_Final_Project/DATA/Shir/Shir1.csv"
+export_path = "/Users/adibnzv/Desktop/DESKTOP/SCHOOL/PhD/Year 1/Semester 2/Python for Biologists/Final Project/PythonCourse_Final_Project/Export"
+min_
 
 ## Main Code
 
@@ -74,4 +82,16 @@ df_arr_shir = df_cleanup(df_shir, "Shir")
 # Create a Joined Freq File and remove lines where base_count is zero
 joined_freq = pd.concat([df_arr_carmel, df_arr_shir])
 joined_freq = joined_freq[joined_freq['base_count'] != 0]
+
+# Create a Mutation DF File
+Mutation_df = get_mut_list(joined_freq)
+
+# Create a Mutation DF File
+Mutation_df = get_mut_list(joined_freq)
+
+# Fileter out using the cutoffs (can be found in the parameters section)
+Mutation_df = get_mut_list(joined_freq)
+
+
+
 
